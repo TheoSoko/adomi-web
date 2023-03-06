563ef160c16fbd662928144f7d5eb80a4e1bc690
TheoSoko <theo.sokolowsky@gmail.com>
2023-03-06 11:36:30 +0100

    Initialisation du projet React avec: TS, MUI, React Router, Axios.

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const renderLogos = () => {
+  let jsxArray:any[] = []
+  for (let i = 0; i < 3; i++){
+    jsxArray.push(<img src="/logo192.png" />)
+  }
+  return jsxArray
+}
+
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Bonjour, et bienvenue sur le site net interweb d'A Do Mi!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          renderLogos()
+        }
       </header>
     </div>
   );
 }
-
-export default App;
